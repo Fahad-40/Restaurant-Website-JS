f01e6493b6c0b829ef94a83bbfd7dfb3eaf1b39a
Fahad <fahad05munir@gmail.com>
2026-04-25 09:58:02 +0500

    reservation functionality

--- JS/reservation.js
+++ JS/reservation.js
@@ -1,8 +1,9 @@
 let dateInput = document.querySelector(`input=[type="date"]`);
 let summaryDate = document.querySelector('.summary-value')
 
-dateInput.addEventListener("click" , () =>{
-
+dateInput.addEventListener("change" , () =>{
+let dateValue = dateInput.ariaValueMax;
+summaryDate.innerText = dateValue;
 })
 
 
