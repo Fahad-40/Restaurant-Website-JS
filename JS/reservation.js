33ba7ba7635b9c4934902c8eb8139213cb939692
Fahad <fahad05munir@gmail.com>
2026-04-25 10:40:59 +0500

    reservation functionality

--- JS/reservation.js
+++ JS/reservation.js
@@ -1,9 +1,34 @@
-let dateInput = document.querySelector(`input=[type="date"]`);
+let dateInput = document.querySelector(`input[type="date"]`);
 let summaryDate = document.querySelector('.summary-value')
 
-dateInput.addEventListener("change" , () =>{
-let dateValue = dateInput.ariaValueMax;
-summaryDate.innerText = dateValue;
+function currentDate() {
+    let now = new Date()
+
+    let today = now.toISOString().split("T")[0];
+    dateInput.value = today;
+
+    let formatedDate2 = now.toLocaleDateString("en-GB", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    })
+
+    summaryDate.innerText = formatedDate2;
+
+}
+currentDate()
+
+dateInput.addEventListener("change", () => {
+    let dateValue = new Date(dateInput.value);
+
+    let formatedDate = dateValue.toLocaleDateString("en-GB", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    })
+
+    summaryDate.innerText = formatedDate;
 })
+
 
 
